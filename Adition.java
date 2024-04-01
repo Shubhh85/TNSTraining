0c8f7ab6153ac32ea089435e76e2aca12a4f8c72
Shubhh85 <156793495+Shubhh85@users.noreply.github.com>
2024-04-01 23:26:46 +0530

    Add files via upload
    ok

---
+++ Adition.java
@@ -0,0 +1,11 @@
+public class Adition {
+    public static void main(String[] args) {
+        int a=4;
+        int b=5;
+        int c=a+b;
+        System.err.println(c);
+
+        
+    }
+    
+}
