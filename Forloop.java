383f22237ae982813dca2378e5d8b06e38cdf291
Shubhh85 <156793495+Shubhh85@users.noreply.github.com>
2024-04-02 12:37:28 +0530

    Add files via upload
    program for For_loop

---
+++ Forloop.java
@@ -0,0 +1,13 @@
+// The forloop is used when we know the number of items we wants to excute a block of code.
+
+/**
+ * Forloop
+ */
+public class Forloop {
+
+    public static void main(String[] args) {
+        for(int i=0;i<101;i++){
+            System.out.println(i);
+        }
+    }
+}
