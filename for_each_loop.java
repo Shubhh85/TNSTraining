7363310878bf82a79eec4bda86c6496a690237a6
Shubhh85 <156793495+Shubhh85@users.noreply.github.com>
2024-04-02 13:03:41 +0530

    Add files via upload
    Program for for_each_loop

---
+++ for_each_loop.java
@@ -0,0 +1,24 @@
+public class for_each_loop {
+    public static void main(String[] args) {
+        int a []={10,20,30,40,50};
+        System.out.println(a);
+
+        for(int i:a){
+            System.out.println(i);
+        }
+        char ch[]={'j','a','v','a'};
+        System.out.println(ch);
+        
+        for (char c: ch){
+            System.out.println(c);
+        }
+        System.out.println();
+        String s1[]={"java","Programing","Learning"};
+        System.out.println(s1);
+
+        for(String s:s1){
+            System.out.println(s);
+        }
+    }
+    
+}
