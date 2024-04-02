9c26e7b6eccc340de9295c59c37081052b2d6991
Shubhh85 <156793495+Shubhh85@users.noreply.github.com>
2024-04-02 12:42:00 +0530

    Add files via upload
    Program for do_while_loop

---
+++ do_while_loop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * do_while_loop
+ */
+public class do_while_loop {
+
+    public static void main(String[] args) {
+        int i=2;
+
+        do{
+            i=i+2;
+            System.out.println(i);
+        }
+        while(i<20);
+    }
+}
